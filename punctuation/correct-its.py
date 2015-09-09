990e2dc8ae1e383593eff74e4ba1577db3e142fa
April Shen <april.tuesday@gmail.com>
2015-09-09 08:09:46 -0700

    starter files for punctuation corrector

---
+++ punctuation/correct-its.py
@@ -0,0 +1,11 @@
+# April Shen -- 2015-09-08
+# The Punctuation Corrector (https://www.hackerrank.com/challenges/punctuation-corrector-its)
+import nltk
+
+
+def readCorpus():
+	corpusFile = "".join(open("corpus.txt").readLines())
+
+
+if __name__ == "__main__":
+	pass
