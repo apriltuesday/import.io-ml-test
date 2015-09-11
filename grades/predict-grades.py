16414999745d453992b940997ceae4646f744f47
April Shen <april.tuesday@gmail.com>
2015-09-10 19:02:18 -0700

    fixing AdaBoost

--- grades/predict-grades.py
+++ grades/predict-grades.py
@@ -21,6 +21,7 @@ features = {
 	"BusinessStudies": 8
 }
 D = len(features)
+K = 8 # number of grade levels (classes for classification)
 
 # list of data
 data = []
@@ -31,59 +32,30 @@ data = []
 
 def initializeWeights():
     """
-    Initialize all weights.
+    Initialize all weights to be uniform.
     """
     w = 1.0 / len(data)
     for d in data:
         d.weight = w
 
 
-def sampleData(n):
-    """
-    Returns a list of n data instances sampled from data according to
-    the distribution of weights.
-    """
-    sampleList = []
-    for i in range(n):
-        k = random()
-        runningSum = 0.0
-        for d in data:
-            if d in sampleList: # sampling without replacement
-                continue
-            runningSum += d.weight
-            if runningSum > k:
-                sampleList.append(d)
-                break
-    return sampleList
-
-
 def reweight(predictions, error):
     """
     Reweighting. See Schapire 1999.
     """
-    # XXX Fix reweighting....
-    # error of this ensemble
-    #error = 1 - accuracy
     # perturb by epsilon to prevent getting log(0)
     if error == 1:
         error -= 0.00001
     elif error == 0: # did perfectly, don't reweight
         return
     # importance factor
-    alpha = 0.5 * log((1 - error) / error)
+    alpha = log((1.0 - error) / error) + log(K - 1.0)
     # normalization factor (running sum)
     z_t = 0.0
     # update weights (numerator)
     for d, p in zip(data, predictions):
-        if p == d.label:
-            #alpha *= -1
-            # if correctly labeled, set weights to 0 XXXX
-            d.weight = 0.0
-            continue
-        d.weight = d.weight * exp(alpha)
+        d.weight *= exp(alpha * (1 if p != d.label else 0))
         z_t += d.weight
-    if z_t == 0:
-        return
     # normalize, to get a probability distribution
     for d in data:
         d.weight /= z_t
@@ -94,8 +66,11 @@ def reweight(predictions, error):
 ###################### MAIN ######################
 
 
-# parse JSON input (either training or test)
 def parseInput(filename):
+	"""
+	Parse JSON input (either training or test) and store in data.
+	Stores math grade as label, if present.
+	"""
 	content = open(filename).readlines()
 	for line in content[1:]:
 		record = json.loads(line)
@@ -111,48 +86,57 @@ def parseInput(filename):
 				x[features[subject]] = record[subject]
 
 		d = Datum(x)
-		if label:
+		if label: #only training data is labeled
 			d.label = label
-		data.append(d)
-	
+		data.append(d)	
 
 
-# parse test output file
 def parseOutput(filename):
+	"""
+	Parse the test output JSON, and store as data labels.
+	"""
 	content = open(filename).readlines()
 	for l, d in zip(content, data):
 		d.label = int(l)
 
 
-# train stump to classify based on majority class for each
-# value of its root features
-def trainStump(stump, sample):
+# XXX Fix this: should return the stump
+# and choose root based on info gain
+def trainStump():
+	"""
+	Train one decision stump on the weighted data.
+	"""
 	groups = [Counter() for x in range(D)]
-	for d in sample:
-		groups[d.features[stump.root]][d.label] += 1
+	for d in data:
+		groups[d.features[stump.root]][d.label] += d.weight
 	for i in range(D):
 		if len(groups[i]) != 0:
 			biggest =  max(groups[i].items(), key=lambda x: x[1])
 			stump.classes[i] = biggest[0]
 
 
-# train ensemble of decision stumps using AdaBoost
-# return list of (m, a) where m is the base model and a is its weight
+def weightedError(predictions):
+	"""
+	Calculate weighted error of predictions.
+	"""
+	return sum([d.weight * (1 if d.label != p else 0) for d,p in zip(data, predictions)])
+
+
 def train():
+	"""
+	Train ensemble of decision stumps using (multiclass) AdaBoost.
+	Return list of (m, a) where m is the decision stump and a is its
+	weight in the final ensemble.
+	"""
 	ensemble = []
 	initializeWeights()
 
 	for i in range(D):
-		# new base learner
-		stump = DecisionStump(i, 8) #8 is number of possible grades
-		# sample
-		sample = sampleData(10) # XXX how many samples?
-		# train base learner on sample
-		trainStump(stump, sample)
-		# predict for all data
+		# train new base learner
+		stump = trainStump()
+		# make predictions and comput error for this learner
 		predictions = [stump.classify(d) for d in data]
-		# calculate error
-		error = sum([d.weight * (1 if d.label != p else 0) for d,p in zip(data, predictions)])
+		error = weightedError(predictions)
 		# reweight
 		alpha = reweight(predictions, error)
 		# add to ensemble
@@ -160,13 +144,28 @@ def train():
 
 	return ensemble
 
-	
 
 def test(ensemble):
-	pass
+	"""
+	Test performance of ensemble on data, returning
+	classification accuracy.
+	"""
+	initializeWeights()
+	predictions = []
+	for d in data:
+		probs = [(a if d.label == m.classify(d) else 0) for m,a in ensemble]
+		predictions.append(max(range(1,8), key = lambda i : probs[i]))
+	print(predictions)
+	return 1.0 - weightedError(predictions)
+
 
 if __name__ == '__main__':
 	parseInput("small/training.json")
 	ensemble = train()
+	data = []
+	parseInput("small/test.in.json")
+	parseOutput("small/test.out.json")
+	accuracy = test(ensemble)
 	for stump, alpha in ensemble:
 		print(alpha, stump.root, stump.classes)
+	print(accuracy)
